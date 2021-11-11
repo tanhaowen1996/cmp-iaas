ce5a162fcf1dbc44c4a67b5d28df2c8a81925b70
谭浩文 <tanhaowen@tanhaowendeMacBook-Pro.local>
2021-11-11 09:22:59 +0800

    add volume urls

--- djapp/djapp/urls.py
+++ djapp/djapp/urls.py
@@ -18,13 +18,14 @@ from django.conf.urls import re_path
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from .views import NetworkViewSet, PortViewSet, KeypairViewSet, ImageViewSet
+from .views import NetworkViewSet, PortViewSet, KeypairViewSet, ImageViewSet, VolumeViewSet
 
 router = SimpleRouter(trailing_slash=False)
 router.register(r'network', NetworkViewSet, basename='network')
 router.register(r'port', PortViewSet, basename='port')
 router.register(r'keypair', KeypairViewSet, basename='keypair')
 router.register(r'image', ImageViewSet, basename='image')
+router.register(r'volume', VolumeViewSet, basename='volume')
 
 
 urlpatterns = [
