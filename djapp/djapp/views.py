093ae26b0724df8e903d226ec6b4505c986fb805
谭浩文 <tanhw2@yonghui.cn>
2021-11-25 17:41:20 +0800

    change volume list status

--- djapp/djapp/views.py
+++ djapp/djapp/views.py
@@ -577,12 +577,27 @@ class VolumeViewSet(OSCommonModelMixin, viewsets.ModelViewSet):
                 volume = instance.get_volume(request.os_conn)
                 if instance.status == volume.status:
                     continue
-                serializer = UpdateVolumeSerializer(instance, data=volume)
-                serializer.is_valid(raise_exception=True)
-                serializer.save(
-                    cluster_name=volume.host,
-                    attachments=volume.attachments,
-                )
+                if volume.attachments == []:
+                    serializer = UpdateVolumeSerializer(instance, data=volume)
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save(
+                        cluster_name=volume.host,
+                        attachments=volume.attachments,
+                        device=None,
+                        server_name=None,
+                        server_id=None,
+                    )
+                else:
+                    server = instance.get_server(request.os_conn, volume.attachments[0].get('server_id'))
+                    serializer = UpdateVolumeSerializer(instance, data=volume)
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save(
+                        cluster_name=volume.host,
+                        attachments=volume.attachments,
+                        device=volume.attachments[0].get('device'),
+                        server_name=server.name,
+                        server_id=volume.attachments[0].get('server_id'),
+                    )
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
         serializer = self.get_serializer(queryset, many=True)
