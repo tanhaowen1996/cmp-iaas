a3e9f37b49f51b0dd0072a0846c6ffe0a4bb4c6a
谭浩文 <tanhw2@yonghui.cn>
2021-11-30 18:06:01 +0800

    add operation log

--- djapp/djapp/urls.py
+++ djapp/djapp/urls.py
@@ -22,6 +22,7 @@ from .views import NetworkViewSet, PortViewSet, KeypairViewSet, ImageViewSet, \
     VolumeViewSet, VolumeTypeViewSet
 from resource_application.views import ResourceApplicationViewSet
 from message_board.views import TopicViewSet
+from operation_log.views import OperationLogViewSet
 
 router = SimpleRouter(trailing_slash=False)
 router.register(r'network', NetworkViewSet, basename='network')
@@ -30,6 +31,7 @@ router.register(r'keypair', KeypairViewSet, basename='keypair')
 router.register(r'image', ImageViewSet, basename='image')
 router.register(r'volume', VolumeViewSet, basename='volume')
 router.register(r'volume_type', VolumeTypeViewSet, basename='volume_type')
+router.register(r'operation', OperationLogViewSet, basename='operation')
 
 router.register(r'resource-application', ResourceApplicationViewSet, basename='resource_application')
 router.register(r'message-board', TopicViewSet, basename='message_board')
