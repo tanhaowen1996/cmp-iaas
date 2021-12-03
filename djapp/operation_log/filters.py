84689709875af115456e505e1c975be121fdeebd
谭浩文 <tanhw2@yonghui.cn>
2021-12-03 14:01:36 +0800

    change filters

--- djapp/operation_log/filters.py
+++ djapp/operation_log/filters.py
@@ -7,12 +7,9 @@ from .models import OperationLog
 class OperationLogFilter(FilterSet):
     name = CharFilter(field_name='name', lookup_expr='icontains')
     user_name = CharFilter(field_name='user_name', lookup_expr='icontains')
-    server_name = CharFilter(field_name='server_name', lookup_expr='icontains')
-    server_id = CharFilter(field_name='server_id', lookup_expr='icontains')
-    volume_id = CharFilter(field_name='volume_id', lookup_expr='icontains')
-    volume_name = CharFilter(field_name='volume_name', lookup_expr='icontains')
+    type_name = CharFilter(field_name='type_name', lookup_expr='icontains')
     created_at = DateTimeFilter(field_name='created_at', lookup_expr='icontains')
 
     class Meta:
         mode = OperationLog
-        filter = ('name', 'user_name', 'server_name', 'server_id', 'volume_id', 'volume_name', 'created_at')
+        filter = ('name', 'user_name', 'type_name', 'created_at')
