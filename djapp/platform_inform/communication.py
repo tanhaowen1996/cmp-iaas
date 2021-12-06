08ce8c2156f99e6c8573233dece0c3e9251fd30a
刘 灿 <liucan2@yonghui.cn>
2021-12-06 18:04:05 +0800

    Update communication.py url-informtation

--- djapp/platform_inform/communication.py
+++ djapp/platform_inform/communication.py
@@ -1,29 +1,41 @@
 import json
 import requests
-auth_url = 'http://itservice-prod.it-service-gateway.gw.yonghui.cn/auth/secret/user'
-sms_url = 'http://itservice-prod.it-service-gateway.gw.yonghui.cn/?Action=market&Service=sms'
-email_url = 'http://itservice-prod.it-service-gateway.gw.yonghui.cn/?Action=sendMail&Service=mail'
-header = {
-    'Content-Type': 'application/json'
-}
-auth_key = {
-    "secretId": "2c2a03cd64fd48ff9c28e70e40361071",
-    "secretKey": "1c92ac73943c49bfb1c0e0f351fe47ee"
-}
-auth_res = requests.post(url=auth_url, data=json.dumps(auth_key), headers=header)
-key = json.loads(auth_res.text)['result']
-key.update(header)
 
 
 class Communication:
     def sms(self, content_list):
+        self.auth_url = 'http://10.214.214.3:10080/auth/secret/user'
+        self.sms_url = 'http://10.214.214.3:10080/?Action=market&Service=sms'
+        header = {
+            'Content-Type': 'application/json'
+        }
+        auth_key = {
+            "secretId": "2c2a03cd64fd48ff9c28e70e40361071",
+            "secretKey": "1c92ac73943c49bfb1c0e0f351fe47ee"
+        }
+        auth_res = requests.post(url=self.auth_url, data=json.dumps(auth_key), headers=header)
+        key = json.loads(auth_res.text)['result']
+        key.update(header)
         sms_data = {
             "sign": '永辉超市',
             "smsList": content_list,
         }
-        requests.post(url=sms_url, headers=key, data=json.dumps(sms_data))
+        a = requests.post(url=self.sms_url, headers=key, data=json.dumps(sms_data))
+        print(a.text)
 
     def email(self, subject, content, to):
+        auth_url = 'http://10.214.214.3:10080/auth/secret/user'
+        email_url = 'http://10.214.214.3:10080/?Action=sendMail&Service=mail'
+        header = {
+            'Content-Type': 'application/json'
+        }
+        auth_key = {
+            "secretId": "2c2a03cd64fd48ff9c28e70e40361071",
+            "secretKey": "1c92ac73943c49bfb1c0e0f351fe47ee"
+        }
+        auth_res = requests.post(url=auth_url, data=json.dumps(auth_key), headers=header)
+        key = json.loads(auth_res.text)['result']
+        key.update(header)
         email_data = {
             "subject": subject,
             "content": content,
