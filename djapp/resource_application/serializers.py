6e1a5a76bfe7cb5c46a3285da52cc231a89c98cf
陈勇 <80752635@yonghui.cn>
2021-12-31 16:34:35 +0800

    add approve_for_network resource application api

--- djapp/resource_application/serializers.py
+++ djapp/resource_application/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from djapp.models import Network
 from .models import ResourceApplication
 
 
@@ -44,3 +45,46 @@ class ResourceApplicationConfirmationSerializer(serializers.ModelSerializer):
         ):
             raise serializers.ValidationError(f"the status is already {self.instance.status}")
         return data
+
+
+class ResourceApplicationNetworkConfirmationSerializer(ResourceApplicationConfirmationSerializer):
+    network_id = serializers.UUIDField()
+    network_name = serializers.CharField(max_length=255)
+    network_description = serializers.CharField(max_length=1024, allow_blank=True, required=False)
+
+    class Meta:
+        model = ResourceApplication
+        fields = (
+            'network_id', 'network_name', 'network_description'
+        )
+
+    def validate_network_id(self, value):
+        try:
+            self.network = Network.objects.get(
+                id=value,
+                category=self.instance.content[0]['category'],
+                is_shared=False)
+        except (KeyError, IndexError) as exc:
+            raise serializers.ValidationError(
+                f'network application content missing { exc }: { self.instance.content }')
+        except Network.DoesNotExist:
+            raise serializers.ValidationError(
+                f"network { value } not found (category type, shared or not)")
+        else:
+            return value
+
+    def save(self):
+        self.network.update_os_network_subnet(
+            self.context['request'].os_conn,
+            name=self.validated_data['network_name'],
+            description=self.validated_data.get('network_description'))
+        self.network.name = self.validated_data['network_name']
+        if self.validated_data.get('network_description'):
+            self.network.description = self.validated_data['network_description']
+
+        if self.instance.tenant not in self.network.tenants:
+            self.network.tenants.append(self.instance.tenant)
+
+        self.network.save()
+        self.instance.status = self.instance.STATUS_APPROVED
+        self.instance.save()
