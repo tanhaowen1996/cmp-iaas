deaf3bd01efd26544f3fe8364082e06d3a188858
刘 灿 <liucan2@yonghui.cn>
2022-03-03 14:46:57 +0800

    Update platform-inform filters.py

--- djapp/platform_inform/filters.py
+++ djapp/platform_inform/filters.py
@@ -1,6 +1,8 @@
 from django_filters import (
     FilterSet,
-    CharFilter
+    CharFilter,
+    OrderingFilter,
+    DateFilter,
 
 )
 from .models import Inform
@@ -9,8 +11,15 @@ from .models import Inform
 class PlatformInformFilter(FilterSet):
     name = CharFilter(field_name='name', lookup_expr='icontains')
     inform_object_type = CharFilter(field_name='inform_object_type', lookup_expr='icontains')
-    created = CharFilter(field_name='created', lookup_expr='icontains')
+    created_date_from = DateFilter(field_name='created__date', lookup_expr='gte')
+    created_date_to = DateFilter(field_name='created__date', lookup_expr='lte')
+    o = OrderingFilter(fields=(
+        ('created', 'created'),
+    ), choices=(
+        ('created', 'created asc'),
+        ('-created', 'created desc'),
+    ))
 
     class Meta:
         model = Inform
-        fields = ('name', 'inform_object_type', 'created')
+        fields = ('name', 'inform_object_type',)
